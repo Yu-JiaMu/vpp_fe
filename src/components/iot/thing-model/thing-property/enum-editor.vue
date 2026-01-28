8f4138615d42e25c418d94bdf7b4b67c6c804c9e
zhuzeen <2642376607@qq.com>
2026-01-28 15:18:55 +0800

    🌈 style: 添加枚举文案修改

--- src/components/iot/thing-model/thing-property/enum-editor.vue
+++ src/components/iot/thing-model/thing-property/enum-editor.vue
@@ -63,10 +63,10 @@
 
         <div
           v-if="modelValue.length === 0 || !hasRegisterDevice"
-          class="text-sm text-gray-400 cursor-pointer hover:text-blue-500"
+          class="text-sm cursor-pointer text-theme hover:text-blue-500"
           @click="add"
         >
-          + 点击添加一项
+          + 点击添加
         </div>
       </div>
     </el-form-item>
